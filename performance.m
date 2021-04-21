eea8cdbb0cb2caf0fc99e9d64e43ddd261f75a3a
AtomXT <xu-tong@outlook.com>
2021-04-21 16:13:58 +0800

    feat(performance): add performance profile plot

--- performance.m
+++ performance.m
@@ -22,14 +22,31 @@ clc
 
 methods = {'GradientDescent', 'GradientDescentW', 'Newton', 'NewtonW', 'BFGS', 'BFGSW', 'DFP', 'DFPW','TRNewtonCG', 'TRSR1CG'};
 
+% set options
+options.term_tol = 1e-6;
+options.max_iterations = 1e3;
+options.c_1_ls = 1e-4;
+options.c_2_ls = 0.9;
+options.c_1_tr = 0.25;
+options.c_2_tr = 0.75;
+options.term_tol_CG = 1e-6;
+options.max_iterations_CG = 1e3;
+options.eta_0 = 0.01;
+options.alpha = 2.0;
+options.rho = 0.5;
+options.beta = 1e-2;
+options.Delta = 0.1;
+options.epsilon = 1e-6;
+
+% for SR1
+options.eta = 1e-3;
+options.r = 0.5;
+
 
 results = [];
 f_trajectory = {};
 count = 1;
 for i=1:12
-    % set options
-    options.term_tol = 1e-6;
-    options.max_iterations = 1e3;
     for j=1:length(methods)
         method.name = methods{j};
         disp("solving problem " + i + " with algorithm " + method.name)
@@ -48,16 +65,64 @@ for i=1:10
     need_k(:,i) = results(i:10:end, 1);
 end
 
+need_k1 = zeros(12,10);
+for i=1:10
+    need_k1(:,i) = results(i:10:end, 2);
+end
+
+need_time = zeros(12,10);
+for i=1:10
+    need_time(:,i) = results(i:10:end, 4);
+end
+
 profile = zeros(1000,10);
 for i=1:1000
     profile(i,:) = sum(need_k < i);
 end
 
-plot(1:1000, profile'/12, 'linewidth',2)
-title('Performance Profile')
+profile_k1 = zeros(1000,10);
+for i=1:1000
+    profile_k1(i,:) = sum(need_k1 < i);
+end
+
+profile_time = zeros(1000,10);
+for i=1:1000
+    profile_time(i,:) = sum(need_time < i*1e-4);
+end
+
+color = ['r','g','b','c','m','y','m--',"r--","g--","b--"];
+for i=1:10
+    plot(1:1000, profile(:,i)'/12,color(i), 'linewidth',1);
+    hold on
+end
+title('Performance Profile: iterations')
 xlabel('iterations(k)')
 ylabel('ratio of problems solved')
 legend('GradientDescent', 'GradientDescentW', 'Newton', 'NewtonW', 'BFGS',...
 'BFGSW', 'DFP', 'DFPW','TRNewtonCG', 'TRSR1CG','Location','southeast')
 
 
+color = ['r','g','b','c','m','y','m--',"r--","g--","b--"];
+for i=1:10
+    plot(1:1000, profile_k1(:,i)'/12,color(i), 'linewidth',1);
+    hold on
+end
+title('Performance Profile: function evaluations')
+xlabel('numbers')
+ylabel('ratio of problems solved')
+legend('GradientDescent', 'GradientDescentW', 'Newton', 'NewtonW', 'BFGS',...
+'BFGSW', 'DFP', 'DFPW','TRNewtonCG', 'TRSR1CG','Location','southeast')
+
+color = ['r','g','b','c','m','y','m--',"r--","g--","b--"];
+for i=1:10
+    plot(1:1000, profile_time(:,i)'/12,color(i), 'linewidth',1);
+    hold on
+end
+title('Performance Profile: time')
+xlabel('$10^{-4}$ seconds','Interpreter','latex')
+ylabel('ratio of problems solved')
+legend('GradientDescent', 'GradientDescentW', 'Newton', 'NewtonW', 'BFGS',...
+'BFGSW', 'DFP', 'DFPW','TRNewtonCG', 'TRSR1CG','Location','southeast')
+
+% split the results into each problem
+a = mat2cell(results,10*ones(12,1),4);
